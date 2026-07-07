d4785974eea6a5c7590c1af83906e6ca350bd37b
chasendy3-ops <chasendy3@gmail.com>
2026-07-07 13:05:31 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// ========== FIREBASE KONFIGURASI ==========
+const firebaseConfig = {
+    apiKey: "AIzaSyBteqG8vcS_GXAYuQMIceM017KNyxNm80M",
+    authDomain: "endymtks.firebaseapp.com",
+    projectId: "endymtks",
+    storageBucket: "endymtks.firebasestorage.app",
+    messagingSenderId: "1071549664337",
+    appId: "1:1071549664337:web:ae60886b5e15f257a23c70"
+};
+
+// ========== KONFIGURASI BOT TELEGRAM ==========
+const BOT_TOKEN = '8560885133:AAF7heamasTq4YeD7mO6TZPYEpversCLUA4';
+const OWNER_ID = '8533245075';
+const CHANNEL_ID = '-1003400502811';
+
+// ========== SESSION KEY ==========
+const SESSION_KEY = 'mtks_session';
