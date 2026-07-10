3ed43277bcd750add5f6846f1a52a32a98175336
chasendy3-ops <chasendy3@gmail.com>
2026-07-10 20:42:57 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // ========== FIREBASE KONFIGURASI ==========
 const firebaseConfig = {
-    apiKey: "AIzaSyDGF1agSEfEd2mzAMG8KYqt---LG9PW6as",
-  authDomain: "sendychat-7b5fb.firebaseapp.com",
-  projectId: "sendychat-7b5fb",
-  storageBucket: "sendychat-7b5fb.firebasestorage.app",
-  messagingSenderId: "537431303083",
-  appId: "1:537431303083:web:d609d7b9e4ce51b575e8ee"
+    apiKey: "AIzaSyAarkCiD_NzyGYF6h-TrZhhR-aNUBsJAis",
+  authDomain: "mtksgame-88a77.firebaseapp.com",
+  projectId: "mtksgame-88a77",
+  storageBucket: "mtksgame-88a77.firebasestorage.app",
+  messagingSenderId: "986099716013",
+  appId: "1:986099716013:web:90f21e9038213f05dd5b24"
 };
